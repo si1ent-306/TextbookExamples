699fc22d52e7520bcebe0e1e0ff87329092ced0f
wagner3956 <elliottjwag@hotmail.com>
2024-02-14 10:52:02 -0600

    Never fear or be anxious because the Lord is with you and is looking out for you

---
+++ src/URLFilePractice.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class URLFilePractice {
+    public static void main(String[] args){
+        System.out.println("Enter a url: ");
+        //http://liveexample.pearsoncmg.com/data/Lincoln.txt
+        String URLString = new Scanner(System.in).next();
+        try {
+            URL url = new URL(URLString);
+            int count = 0;
+            Scanner input = new Scanner(url.openStream());
+            while ((input.hasNext())){
+                String line = input.nextLine();
+                count += line.length();
+            }
+            System.out.println("File size " + count);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
